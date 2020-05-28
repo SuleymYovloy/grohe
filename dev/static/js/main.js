782861ee465e749635caa5e05b23a976b3163684
Suleym Yovloy <suleymevlo81@gmail.com>
2020-05-28 16:49:21 +0300

    first commit

---
+++ dev/static/js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+    svg4everybody({});
+    $('.offers-carousel').owlCarousel({
+        loop:true,
+        items: 1,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1 
+            },
+            800:{
+                items:2
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    $('.banner-carousel').owlCarousel({
+        loop:true,
+        items: 1,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1 
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    $('.sections-carousel').owlCarousel({
+        loop:true,
+        items: 1,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1 
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    $('.collections-carousel').owlCarousel({
+        loop:true,
+        items: 1,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1 
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    
+});
